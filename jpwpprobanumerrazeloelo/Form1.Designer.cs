e1b0513cd5f892b1990ee0952c3d2b3361057441
kroliczysyn <s165497@student.pg.gda.pl>
2018-11-07 20:42:40 +0100

    poprawka z 07.11
    dodane powrotbutton

--- jpwpprobanumerrazeloelo/Form1.Designer.cs
+++ jpwpprobanumerrazeloelo/Form1.Designer.cs
@@ -30,6 +30,8 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Orient));
             this.bWyjscie = new System.Windows.Forms.Button();
+            this.bInfo = new System.Windows.Forms.Button();
+            this.PlayButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // bWyjscie
@@ -39,7 +41,7 @@
             this.bWyjscie.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.bWyjscie.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.bWyjscie.ForeColor = System.Drawing.SystemColors.Control;
-            this.bWyjscie.Location = new System.Drawing.Point(521, 169);
+            this.bWyjscie.Location = new System.Drawing.Point(521, 101);
             this.bWyjscie.Name = "bWyjscie";
             this.bWyjscie.Size = new System.Drawing.Size(208, 56);
             this.bWyjscie.TabIndex = 0;
@@ -47,12 +49,39 @@
             this.bWyjscie.UseVisualStyleBackColor = false;
             this.bWyjscie.Click += new System.EventHandler(this.bWyjscie_Click);
             // 
+            // bInfo
+            // 
+            this.bInfo.BackColor = System.Drawing.Color.Transparent;
+            this.bInfo.Cursor = System.Windows.Forms.Cursors.Default;
+            this.bInfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.bInfo.Font = new System.Drawing.Font("Comic Sans MS", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.bInfo.ForeColor = System.Drawing.SystemColors.Control;
+            this.bInfo.Location = new System.Drawing.Point(521, 24);
+            this.bInfo.Name = "bInfo";
+            this.bInfo.Size = new System.Drawing.Size(208, 56);
+            this.bInfo.TabIndex = 1;
+            this.bInfo.Text = "Jak Grać?";
+            this.bInfo.UseVisualStyleBackColor = false;
+            this.bInfo.Click += new System.EventHandler(this.bInfo_Click);
+            // 
+            // PlayButton
+            // 
+            this.PlayButton.Location = new System.Drawing.Point(557, 205);
+            this.PlayButton.Name = "PlayButton";
+            this.PlayButton.Size = new System.Drawing.Size(106, 53);
+            this.PlayButton.TabIndex = 2;
+            this.PlayButton.Text = "Graj";
+            this.PlayButton.UseVisualStyleBackColor = true;
+            this.PlayButton.Click += new System.EventHandler(this.PlayButton_Click);
+            // 
             // Orient
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.ClientSize = new System.Drawing.Size(800, 602);
+            this.Controls.Add(this.PlayButton);
+            this.Controls.Add(this.bInfo);
             this.Controls.Add(this.bWyjscie);
             this.MaximumSize = new System.Drawing.Size(1280, 1024);
             this.Name = "Orient";
@@ -64,6 +93,8 @@
         #endregion
 
         private System.Windows.Forms.Button bWyjscie;
+        private System.Windows.Forms.Button bInfo;
+        private System.Windows.Forms.Button PlayButton;
     }
 }
 
--- jpwpprobanumerrazeloelo/Form1.cs
+++ jpwpprobanumerrazeloelo/Form1.cs
@@ -11,6 +11,7 @@ namespace jpwpprobanumerrazeloelo
 {
     public partial class Orient : Form
     {
+        Button PowrotButton = new Button();
         public Orient()
         {
             InitializeComponent();
@@ -19,6 +20,31 @@ namespace jpwpprobanumerrazeloelo
         private void bWyjscie_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void bInfo_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the Form2 class
+            Form2 settingsForm = new Form2();
+
+            // Show the settings form
+            settingsForm.Show();
+        }
+
+        private void PlayButton_Click(object sender, EventArgs e)
+        {
+            Controls.Remove(bInfo);
+            Controls.Remove(bWyjscie);
+            Controls.Remove(PlayButton);
+            this.Controls.Add(PowrotButton);
+        }
+
+        private void PowrotButton_Click(object sender, EventArgs e)
+        {
+            this.Controls.Add(bInfo);
+            this.Controls.Add(bWyjscie);
+            this.Controls.Add(PlayButton);
+            this.Controls.Remove(PowrotButton);
         }
     }
 }
