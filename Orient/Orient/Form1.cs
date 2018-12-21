12f110ce1bf2574d583b02743298e939f86b71c3
kroliczysyn <s165497@student.pg.gda.pl>
2018-12-21 11:20:30 +0100

    dodano timer

--- Orient/Orient/Form1.cs
+++ Orient/Orient/Form1.cs
@@ -12,7 +12,9 @@ namespace Orient_
     public partial class Form1 : Form
     {
         StartButton SB = new StartButton();
+        BlueBall BB = new BlueBall();
         RedBall RB = new RedBall();
+        Random r = new Random();
         public Form1()
         {
             InitializeComponent();
@@ -22,12 +24,42 @@ namespace Orient_
         public void StartButton_Click(object sender, EventArgs e)
         {
             SB.Visible = false;
-            RB.Location = new Point(300, 300);
-            this.Controls.Add(RB);
+            timer1.Enabled = true;
         }
             private void Form1_Load(object sender, EventArgs e)
         {
 
+        }
+        private int lastBall = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int x, y, numer = 0;
+            x = r.Next(0, 300);
+            y = r.Next(0, 300);
+            numer = r.Next(0, 2);
+            switch (lastBall)
+            {
+                case 0:
+                    this.Controls.Remove(RB);
+                    break;
+                case 1:
+                    this.Controls.Remove(BB);
+                    break;
+            }
+            switch (numer)
+            {
+                case 0:
+                    RB.Location = new Point(x, y);
+                    this.Controls.Add(RB);
+                    lastBall = 0;
+                    break;
+                case 1:
+                    BB.Location = new Point(x, y);
+                    this.Controls.Add(BB);
+                    lastBall = 1;
+                    break;
+            }
+            
         }
     }
 }
